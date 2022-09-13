ed2d72a538d5242c2956d34e6b5be3bde7884359
Salim Audu <audusalim00@gmail.com>
2022-09-13 08:09:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include <main.h>
+#include "holberton.h"
 /**
- * main - Prints Salim
+ * main - Prints Holberto
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('S');
-	_putchar('A');
-	_putchar('L');
-	_putchar('L');
-	_putchar('I');
-	_putchar('L');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');							
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
