b7390cd299e020edfee27646fbb11b50b8059c66
Salim Audu <audusalim00@gmail.com>
2022-09-13 06:19:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "Salim.h"
+/**
+ * main - Prints Salim
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('S');
+	_putchar('A');
+	_putchar('L');
+	_putchar('L');
+	_putchar('I');
+	_putchar('L');
+	_putchar('\n');
+	return (0);
+}
