eca406347fb9482c5d5fc9d9357fcb13a8c055ce
Salim Audu <audusalim00@gmail.com>
2022-09-13 17:16:11 -0700

    0-salim.c

---
+++ 0x02-functions_nested_loops/0-salim.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - function prints text as output
+ *
+ * Return:return 0
+ */
+int main(void)
+{
+	char [8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 7; i++)
+	{
+		_putchar(name[i]);
+	}
+	_putchar(6);
+	return(0);
+	;
+}
