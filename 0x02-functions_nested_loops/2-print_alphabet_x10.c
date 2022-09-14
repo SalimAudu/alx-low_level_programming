8274b501872460ae443f0fe9abc51e18e2430399
Salim Audu <audusalim00@gmail.com>
2022-09-14 10:21:50 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,21 +5,19 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void);
+{
+	int b = 0;
 
+	while (b < 10)
 	{
-		int b = 0;
+		char a = 'a';
 
-		while (b < 10)
-		{
-			char a = 'a';
-
-			while (a <= 'z')
-			{
-				_putchar(a);
-				a++;
-			}
-
-				_putchar('\n');
-				b++;
+		while (a <= 'z')
+		{	
+			_putchar(a);
+			a++;
 		}
+			_putchar('\n');
+				b++;
 	}
+}
