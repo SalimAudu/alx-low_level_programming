4413727395840dc5cf8a8a31b67b95d17907b93a
Salim Audu <audusalim00@gmail.com>
2022-09-15 09:23:32 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isgit - check if parameter c presents a digit.
+ * @c: Int representing an ascii character
+ * Return: Return 1 if it is a digit otherwise return 0.
+ */
+int _isdigit(int c)
+{
+	if (c >=48 && c <= 57)
+	{
+		return (1);
+	}
+		return (0);
+}
