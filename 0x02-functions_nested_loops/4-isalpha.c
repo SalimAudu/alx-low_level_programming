23f49bc4341151c2c8618b9febfa2a388d1ee55a
Salim Audu <audusalim00@gmail.com>
2022-09-15 05:06:55 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - to control if a character is alphabetical
+ *
+ * Return: return 0 or 1
+ */
+int _isalpha(int c)
+{
+	for ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+		return (0);
+}
