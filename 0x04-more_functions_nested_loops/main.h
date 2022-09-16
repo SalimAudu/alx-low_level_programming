9817b0c28edc1eb64435e575789ff70dd646b3ba
Salim Audu <audusalim00@gmail.com>
2022-09-15 19:23:46 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,8 +6,8 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int _isupper(int c);
-int _isdigit(int c);
+void print_triangle(int size);
 int mul(int a, int b);
-int _putchar(int char);
+int _isdigit(int c);
+int _isupper(int c);
 
