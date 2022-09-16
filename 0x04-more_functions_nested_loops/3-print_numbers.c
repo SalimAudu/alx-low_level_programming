cdbb4ad325f783b7e12b1ae8a54e24f8534d0a9b
Salim Audu <audusalim00@gmail.com>
2022-09-15 19:51:23 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - Print numbers between 0 to 9 incl.
+ * print_numbers - Print numbers bettween 0 to 9
  *
  * Return: Void.
  */
@@ -9,9 +9,10 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; 1++)
 	{
 		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
+
