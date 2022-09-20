1db1fddeb571f323a7b44a3e79c2dfeaaea13882
Salim Audu <audusalim00@gmail.com>
2022-09-20 04:05:00 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,1 @@
-#include "main.h"
-/**
- * _strlen - length of a string
- * @s: input char
- * Return: returns lenght;
- */
-int _strlen(char *s)
-{
-	int l = 0;
-	while (*s != '\0')
-	{
-		s++;
-		l++;
-	}
-	return (l);
-}
+#include "main.h
