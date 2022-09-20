19b24be0f07074eba54dd7cb85a8f89717fd2d3a
Salim Audu <audusalim00@gmail.com>
2022-09-20 07:49:09 -0700

    revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,12 +6,11 @@
 void rev_string(char *s)
 {
 	int i =0, length;
-	length = _strlen(s_=) - 1;
-	while (lenght > i)
+	length = _strlen(s) - 1;
+	while (length > i)
 	{
 		swap_char(s + length, s + i);
-		i++'
-			i++;
+		i++;
 		length--;
 	}
 }
